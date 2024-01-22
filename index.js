f21cffbc6ed4fcd091e26fc456aae601b6095540
Restyled.io <commits@restyled.io>
2024-01-22 08:32:32 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const rockBtn = document.querySelector('#rock');
-const paperBtn = document.querySelector('#paper');
-const scissorsBtn = document.querySelector('#scissors');
-let resultBoard = document.querySelector('.resultBoard');
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+let resultBoard = document.querySelector(".resultBoard");
 
-let resultRoundDiv = document.createElement('div');
-let scoreDiv = document.createElement('div');
-let resultRound = document.createElement('p');
+let resultRoundDiv = document.createElement("div");
+let scoreDiv = document.createElement("div");
+let resultRound = document.createElement("p");
 
-let playerScore = document.createElement('p');
-let computerScore = document.createElement('p');
+let playerScore = document.createElement("p");
+let computerScore = document.createElement("p");
 
-const resetBtn = document.createElement('button');
-resetBtn.textContent = 'Try Again';
+const resetBtn = document.createElement("button");
+resetBtn.textContent = "Try Again";
 
 let result = null;
 let player = 0;
 let computer = 0;
 
 function getComputerChoice() {
-  let choice = [ "rock", "paper", "scissors" ];
+  let choice = ["rock", "paper", "scissors"];
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-
-  if ((playerSelection === 'scissors' && computerSelection === 'rock') ||
-      (playerSelection === 'rock' && computerSelection === 'paper') ||
-      (playerSelection === 'paper' && computerSelection === 'scissors')) {
-    resultRound.textContent =
-        `You Lose! ${computerSelection} beats ${playerSelection}`;
-    resultRoundDiv.innerHTML = '';
+  if (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors")
+  ) {
+    resultRound.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    resultRoundDiv.innerHTML = "";
     resultRoundDiv.appendChild(resultRound);
     return false;
   }
@@ -39,38 +39,37 @@ function playRound(playerSelection, computerSelection) {
     return playRound(playerSelection, computerSelection);
   }
 
-  resultRound.textContent =
-      `You Win! ${playerSelection} beats ${computerSelection}`;
-  resultRoundDiv.innerHTML = '';
+  resultRound.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+  resultRoundDiv.innerHTML = "";
   resultRoundDiv.appendChild(resultRound);
   return true;
 }
 
-rockBtn.addEventListener('click', () => {
-  result = playRound('rock', getComputerChoice());
+rockBtn.addEventListener("click", () => {
+  result = playRound("rock", getComputerChoice());
   tallyPoints();
 });
 
-paperBtn.addEventListener('click', () => {
-  result = playRound('paper', getComputerChoice());
+paperBtn.addEventListener("click", () => {
+  result = playRound("paper", getComputerChoice());
   tallyPoints();
 });
 
-scissorsBtn.addEventListener('click', () => {
-  result = playRound('scissors', getComputerChoice());
+scissorsBtn.addEventListener("click", () => {
+  result = playRound("scissors", getComputerChoice());
   tallyPoints();
 });
 
-resetBtn.addEventListener('click', () => {
+resetBtn.addEventListener("click", () => {
   result = null;
   player = 0;
   computer = 0;
-  resultRoundDiv.innerHTML = '';
-  scoreDiv.innerHTML = '';
+  resultRoundDiv.innerHTML = "";
+  scoreDiv.innerHTML = "";
 
-  rockBtn.style.display = 'inline';
-  paperBtn.style.display = 'inline';
-  scissorsBtn.style.display = 'inline';
+  rockBtn.style.display = "inline";
+  paperBtn.style.display = "inline";
+  scissorsBtn.style.display = "inline";
 
   game();
 });
@@ -85,25 +84,25 @@ function tallyPoints() {
   playerScore.textContent = `Player: ${player}`;
   computerScore.textContent = `Computer: ${computer}`;
 
-  scoreDiv.innerHTML = '';
+  scoreDiv.innerHTML = "";
   scoreDiv.appendChild(playerScore);
   scoreDiv.appendChild(computerScore);
 
   if (player === 5 || computer === 5) {
-    let winner = (player === 5) ? 'Player' : 'Computer';
+    let winner = player === 5 ? "Player" : "Computer";
     resultRound.textContent = `${winner} Wins The Match!`;
-    resultRoundDiv.innerHTML = '';
+    resultRoundDiv.innerHTML = "";
     resultRoundDiv.appendChild(resultRound);
     resultBoard.appendChild(resetBtn);
 
-    rockBtn.style.display = 'none';
-    paperBtn.style.display = 'none';
-    scissorsBtn.style.display = 'none';
+    rockBtn.style.display = "none";
+    paperBtn.style.display = "none";
+    scissorsBtn.style.display = "none";
   }
 }
 
 function game() {
-  resultBoard.innerHTML = '';
+  resultBoard.innerHTML = "";
   resultBoard.appendChild(resultRoundDiv);
   resultBoard.appendChild(scoreDiv);
 }
