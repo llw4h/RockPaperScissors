8d6e6fdf6a5b88f4fe7c826526bfd0269719f332
Restyled.io <commits@restyled.io>
2024-01-22 08:32:31 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -17,95 +17,95 @@ let result = null;
 let player = 0;
 let computer = 0;
 
-function getComputerChoice(){
-    let choice = ["rock", "paper", "scissors"];
-    return choice[Math.floor(Math.random() * choice.length)] ;
+function getComputerChoice() {
+  let choice = [ "rock", "paper", "scissors" ];
+  return choice[Math.floor(Math.random() * choice.length)];
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
 
-    if ((playerSelection === 'scissors' && computerSelection === 'rock') ||
-    (playerSelection === 'rock' && computerSelection === 'paper') ||
-    (playerSelection === 'paper' && computerSelection === 'scissors')){
-        resultRound.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
-        resultRoundDiv.innerHTML = '';  
-        resultRoundDiv.appendChild(resultRound);
-        return false;
-    } 
-
-    if (playerSelection === computerSelection) {
-        computerSelection = getComputerChoice();
-        return playRound(playerSelection, computerSelection);
-    }
-
-    resultRound.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+  if ((playerSelection === 'scissors' && computerSelection === 'rock') ||
+      (playerSelection === 'rock' && computerSelection === 'paper') ||
+      (playerSelection === 'paper' && computerSelection === 'scissors')) {
+    resultRound.textContent =
+        `You Lose! ${computerSelection} beats ${playerSelection}`;
     resultRoundDiv.innerHTML = '';
     resultRoundDiv.appendChild(resultRound);
-    return true;
+    return false;
+  }
+
+  if (playerSelection === computerSelection) {
+    computerSelection = getComputerChoice();
+    return playRound(playerSelection, computerSelection);
+  }
+
+  resultRound.textContent =
+      `You Win! ${playerSelection} beats ${computerSelection}`;
+  resultRoundDiv.innerHTML = '';
+  resultRoundDiv.appendChild(resultRound);
+  return true;
 }
 
 rockBtn.addEventListener('click', () => {
-    result = playRound('rock', getComputerChoice());
-    tallyPoints();
+  result = playRound('rock', getComputerChoice());
+  tallyPoints();
 });
 
 paperBtn.addEventListener('click', () => {
-    result = playRound('paper', getComputerChoice());
-    tallyPoints();
+  result = playRound('paper', getComputerChoice());
+  tallyPoints();
 });
 
 scissorsBtn.addEventListener('click', () => {
-    result = playRound('scissors', getComputerChoice());
-    tallyPoints();
+  result = playRound('scissors', getComputerChoice());
+  tallyPoints();
 });
 
 resetBtn.addEventListener('click', () => {
-    result = null;
-    player = 0;
-    computer = 0;
-    resultRoundDiv.innerHTML = '';
-    scoreDiv.innerHTML = '';
-    
-    rockBtn.style.display = 'inline'; 
-    paperBtn.style.display = 'inline';
-    scissorsBtn.style.display = 'inline';
+  result = null;
+  player = 0;
+  computer = 0;
+  resultRoundDiv.innerHTML = '';
+  scoreDiv.innerHTML = '';
 
-    game();
+  rockBtn.style.display = 'inline';
+  paperBtn.style.display = 'inline';
+  scissorsBtn.style.display = 'inline';
 
+  game();
 });
 
-function tallyPoints(){
-    if (result === true) {
-        player++;
-    } else {
-        computer++;
-    }
+function tallyPoints() {
+  if (result === true) {
+    player++;
+  } else {
+    computer++;
+  }
 
-    playerScore.textContent = `Player: ${player}`;
-    computerScore.textContent = `Computer: ${computer}`;
+  playerScore.textContent = `Player: ${player}`;
+  computerScore.textContent = `Computer: ${computer}`;
 
-    scoreDiv.innerHTML = '';
-    scoreDiv.appendChild(playerScore);
-    scoreDiv.appendChild(computerScore);
+  scoreDiv.innerHTML = '';
+  scoreDiv.appendChild(playerScore);
+  scoreDiv.appendChild(computerScore);
 
-    if (player === 5 || computer === 5){
-        let winner = (player === 5) ? 'Player' : 'Computer';
-        resultRound.textContent = `${winner} Wins The Match!`;
-        resultRoundDiv.innerHTML = '';
-        resultRoundDiv.appendChild(resultRound);
-        resultBoard.appendChild(resetBtn);
+  if (player === 5 || computer === 5) {
+    let winner = (player === 5) ? 'Player' : 'Computer';
+    resultRound.textContent = `${winner} Wins The Match!`;
+    resultRoundDiv.innerHTML = '';
+    resultRoundDiv.appendChild(resultRound);
+    resultBoard.appendChild(resetBtn);
 
-        rockBtn.style.display = 'none'; 
-        paperBtn.style.display = 'none';
-        scissorsBtn.style.display = 'none';
-    }
+    rockBtn.style.display = 'none';
+    paperBtn.style.display = 'none';
+    scissorsBtn.style.display = 'none';
+  }
 }
 
-function game(){
-    resultBoard.innerHTML = '';  
-    resultBoard.appendChild(resultRoundDiv);
-    resultBoard.appendChild(scoreDiv);
+function game() {
+  resultBoard.innerHTML = '';
+  resultBoard.appendChild(resultRoundDiv);
+  resultBoard.appendChild(scoreDiv);
 }
-
 
 game();
